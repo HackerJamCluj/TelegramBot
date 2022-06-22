1a53c8d0270866e182d6bcb4749d059e6a60ecb2
Mihai Costea <macostea@users.noreply.github.com>
2022-06-22 12:32:16 +0300

    Update code to match worksheet

--- basic_bot.py
+++ basic_bot.py
@@ -6,28 +6,30 @@ import datetime
 
 import RPi.GPIO as GPIO
 
+LED_PIN = 19
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
 
-    print 'Got command: %s' % command
+    print('Got command: %s' % command)
 
     if command == '/roll':
         bot.sendMessage(chat_id, random.randint(1,6))
-    elif command == '/lightTurnOn':
-        GPIO.output(18,GPIO.HIGH)
-    elif command == '/lightTurnOff':
-         GPIO.output(18,GPIO.LOW)
+    elif command == '/turnLightOn':
+        GPIO.output(LED_PIN, GPIO.HIGH)
+    elif command == '/turnLightOff':
+         GPIO.output(LED_PIN, GPIO.LOW)
 
 if __name__ == '__main__':
 
     bot = telepot.Bot('YOUR TOKEN HERE')
     bot.message_loop(handle)
-    print 'I am listening'
+    print('I am listening')
 
     while 1:
         time.sleep(10)
